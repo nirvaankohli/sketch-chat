72b81cacd126584ef1ab87da553db3d06d99e9db
Nirvaan Kohli <nirvaan.kohli@gmail.com>
2025-10-13 17:56:05 -0400

    req update + init paths

--- app.py
+++ app.py
@@ -1,1 +1,15 @@
 from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+
+@app.route("/", methods=["GET"])
+def home():
+
+    return "Welcome to chatting app thing!"
+
+if __name__ == "__main__":
+
+    app.run(debug=True)
+
+    
