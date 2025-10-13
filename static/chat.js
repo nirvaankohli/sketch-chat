1744c831e89f859dfbb1fd4876b2c22c05620e10
Nirvaan Kohli <nirvaan.kohli@gmail.com>
2025-10-13 18:42:56 -0400

    Enhance doesCookieExistOrNot function to accept expiration days and value parameters

--- static/chat.js
+++ static/chat.js
@@ -4,8 +4,24 @@ socket.on("connect", () => {
   console.log("DEBUG: Connected to server 🔌");
 });
 
-function doesCookieExistOrNot(cookieName) {
+function doesCookieExistOrNot(cookieName, howmanydays, value) {
+  if (!getCookie(cookieName)) {
+    // REMOVE LATER
+
+    console.log(
+      `DEBUG: Cookie "${cookieName}" does not exist. Creating a new one...`
+    );
+
+    const d = new Date();
     
+    d.setTime(d.getTime() + (howmanydays * 24 * 60 * 60 * 1000));
+    
+    const expires = "expires=" + d.toUTCString();
+    
+    document.cookie = cookieName + "=" + value + ";" + expires + ";path=/";
+
+
+  }
 }
 
 socket.on(
