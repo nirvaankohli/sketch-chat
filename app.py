949e72dbd95cacf544a26a8a458fee64213d542b
Nirvaan Kohli <nirvaan.kohli@gmail.com>
2025-10-13 18:16:21 -0400

    import os get key

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from flask import Flask, jsonify, request
 from flask_socketio import SocketIO, send
 from pythondotenv import load_dotenv
+import os
 
 load_dotenv()
 
 app = Flask(__name__)
 
+app.config["SECRET_KEY"] = 
 
 @app.route("/", methods=["GET"])
 def home():
